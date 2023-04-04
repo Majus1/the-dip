91086e37686f1d152b1ecc42594a9329356da05d
Maj <59331721+Majus1@users.noreply.github.com>
2023-04-04 11:03:20 +0200

    Indicator dots now follow slider logic

--- js/carousel.js
+++ js/carousel.js
@@ -1,32 +1,130 @@
+// ::::: Dot Possition check :::::
+
+/*
+Works great with the autoSlide they work together
+to allow the user a good navigation experience while
+using the application.
+*/
+
+let indicatorDots = document.querySelectorAll(".dot");
+
+const dotPossitionCheck = () => {
+    if (counterValue === 0) {
+        
+        // Removes filled in dot form all indicator dots
+        let indicatorDots = document.querySelectorAll(".dot");
+        for (i=0; i< indicatorDots.length; i++) {
+            indicatorDots[i].classList.add("inactivated-dot");
+        }
+
+        // Makes dot coresponting to the counterValue active
+        workoutOption = document.querySelector("#first-dot");
+        workoutOption.classList.add("activated-dot");
+        workoutOption.classList.remove("inactivated-dot");
+
+        return
+
+    } else if (counterValue === 1) {
+        
+        // Removes filled in dot form all indicator dots
+        let indicatorDots = document.querySelectorAll(".dot");
+        for (i=0; i< indicatorDots.length; i++) {
+            indicatorDots[i].classList.add("inactivated-dot");
+        }
+
+        // Makes dot coresponting to the counterValue active
+        workoutOption = document.querySelector("#second-dot");
+        workoutOption.classList.add("activated-dot");
+        workoutOption.classList.remove("inactivated-dot");
+
+        return
+
+    } else if (counterValue === 2) {
+        
+        // Removes filled in dot form all indicator dots
+        let indicatorDots = document.querySelectorAll(".dot");
+        for (i=0; i< indicatorDots.length; i++) {
+            indicatorDots[i].classList.add("inactivated-dot");
+        }
+
+        // Makes dot coresponting to the counterValue active
+        workoutOption = document.querySelector("#third-dot");
+        workoutOption.classList.add("activated-dot");
+        workoutOption.classList.remove("inactivated-dot");
+
+        return
+
+    } else if (counterValue === 3) {
+        
+        // Removes filled in dot form all indicator dots
+        let indicatorDots = document.querySelectorAll(".dot");
+        for (i=0; i< indicatorDots.length; i++) {
+            indicatorDots[i].classList.add("inactivated-dot");
+        }
+
+        // Makes dot coresponting to the counterValue active
+        workoutOption = document.querySelector("#fourth-dot");
+        workoutOption.classList.add("activated-dot");
+        workoutOption.classList.remove("inactivated-dot");
+
+        return
+    }
+}
+
+// ::::: Dot Possition check :::::
+
+
+
 // ::::: The workout option slider :::::
-// ::: Makes carousell scrollable :::::
+
+/*
+- Makes carousel scrollable by looking at the position diff.
+- Allows the user to swipe and select different workout.
+- By swiping left and right the user can choose from a predefined workouts.
+*/
+
+
 const carousel = document.querySelector(".carousel");
 
 let isDragStart = false, prevPageX, prevScrollLeft, positionDiff;
-
 let counterValue = 0 // Sets the value of the counter
 
 const autoSlide = () => {
     // if there is no image left to scroll then return from here.
     if(carousel.scrollLeft == (carousel.scrollWidth - carousel.clientWidth)) return;
 
+    
     positionDiff = Math.abs(positionDiff) // making position Diff alue to positive
     let firstImgWidth = 428; // this value is hard coded but it shouldent be.
     // getting difference value that needs to add or reduce from carousel left to make middle image center
     let valDifference = firstImgWidth - positionDiff;
     
+
+    // if user scrolls right following happens
     if (carousel.scrollLeft > prevScrollLeft) {
-        // if user is scrolling to the right
+        
+        // Changes scrollable carousell icon
         counterValue += 1; // adds one from the counter
         console.log(`User scrolled right and the value of counterValue is ${counterValue}`);
+        
+        // Changes that effect the indicator dot by calling the dotPossitionCheck function.
+        dotPossitionCheck();
+
         return carousel.scrollLeft += positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
-    } else {
+
+    } 
+    
+    // if user scrolls left following happens
+    else {
         // if user is scrolling to the left
         if (counterValue === 0) {
             console.log(`User scrolled left but value cannot be negative`);
         } else {
             counterValue -= 1;
             console.log(`User scrolled left and the value of counterValue is ${counterValue}`);
+
+            // Changes that effect the indicator dot by calling the dotPossitionCheck function.
+            dotPossitionCheck();
         }
         carousel.scrollLeft -= positionDiff > firstImgWidth / 3 ? valDifference : -positionDiff;
     }
@@ -65,11 +163,5 @@ carousel.addEventListener("touchend", dragStop);
 // ::::: Resources :::::
 // https://www.youtube.com/watch?v=7HPsdVQhpRw
 // ::::: Resources :::::
-
-// ::: Makes carousell scrollable :::
-
-// ::: indicator dot logic :::
-
-// ::: indicator dot logic :::
 
 // ::::: The workout option slider :::::
